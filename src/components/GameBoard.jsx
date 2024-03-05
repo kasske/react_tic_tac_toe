4abbd1607d86a73cd7403c49d37a4a65574855dc
DESKTOP-ULKUGE2\Kasske <kastriot.limani@rrota.com>
2024-03-05 23:01:44 +0100

    Tic tac toe game using react js

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,34 @@
+export default function GameBoard({ onSelectSquare, board }) {
+
+//   const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+//   function handleSelectSquare(rowIndex, colIndex) {
+//     setGameBoard((prevGameBoard) => {
+//       /* this is  so we dont mutate state directly (if state is array), but make a copy*/
+//       const updatedBoard = [
+//         ...prevGameBoard.map((innerArray) => [...innerArray]),
+//       ];
+//       updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+
+//       return updatedBoard;
+//     });
+
+//     onSelectSquare();
+//   }
+
+  return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null}>{playerSymbol}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
---
+++ src/components/Player.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol, isActive }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName)
+
+  function handleEditClick() {
+    /* this is how to change state based on previous state */
+    setIsEditing((editing) => !editing );
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value)
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if(isEditing) {
+    editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>;
+  }
+
+
+  return (
+    <li className={isActive ? 'active': undefined}>
+      <span className="player">
+        {editablePlayerName}
+        
+        <span className="player-symbol">{symbol}</span>
+        <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+      </span>
+    </li>
+  );
+}
